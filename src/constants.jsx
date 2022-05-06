cfdf4fc450329fa9df956508f7099f5c1ce61a0e
Sai Lavanya Cheemakurthy <lavanya.sai@accionlabs.com>
2022-05-06 12:53:55 +0530

    Updated DOMAIN_URL to netlify url

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://spintowin-575b1-default-rtdb.asia-southeast1.firebasedatabase.app';
-export const DOMAIN_URL= 'http://localhost:3000'
+export const DOMAIN_URL= 'https://jazzy-puppy-2233f1.netlify.app'
