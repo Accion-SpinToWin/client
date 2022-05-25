3c45eea5d9cb21087e23ded91f09ed71b7b77db5
SantoshAccion <50869168+SantoshAccion@users.noreply.github.com>
2022-05-25 10:34:23 +0530

    style update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,27 +16,8 @@ export const Header = () => {
 
   return (
     <div id="header-li" className="list">
-      <ul>
-        <li className={pathName === '/' ? 'selected-path' : ''}><Link to="/">Main</Link></li>
-        <li className={pathName === '/home' ? 'selected-path' : ''}><Link to="home"> <FontAwesomeIcon icon={faAward} color={'blue'} /></Link></li>
-        <li className={pathName === '/home-selection' ? 'selected-path' : ''}>
-          <Link to="home-selection">
-            <div><FontAwesomeIcon icon={faAward} color={'blue'}
-              style={pathName === '/home-selection' ? { fontSize: '1.1rem' } : { fontSize: '0.7rem' }} />
-            <FontAwesomeIcon icon={faAward} color={'black'}
-              style={pathName === '/home-selection' ? { fontSize: '1.4rem' } : { fontSize: '1.1rem' }} />
-            <FontAwesomeIcon icon={faAward} color={'blue'}
-              style={pathName === '/home-selection' ? { fontSize: '1.1rem' } : { fontSize: '0.7rem' }} />
-         </div> </Link></li>
-        <li className={pathName === '/dashboard' ? 'selected-path' : ''} ><Link to="dashboard">Dashboard</Link></li>
-        <li className={pathName === '/admin' ? 'selected-path' : ''}><Link to="admin">Admin</Link></li>
-        {/* <li className={pathName === '/about' ? 'selected-path' : ''}><Link to="about">About</Link></li> */}
-
-      </ul>
-      <div className='last'>
-        <div className='last-btn' style={{ color: 'black', fontWeight: '700' }} onClick={() => navigate('/')}>
+      <div style={{marginLeft: 'auto', marginRight: '20px'}}>
           <img src='https://www.austintechnologycouncil.org/wp-content/uploads/2019/07/Accion-Logo.png' width={100} /></div>
-      </div>
     </div>
   )
 }
